56d2e46e957007d279f6c88eedf2c54a40ef6e7c
rebeca-august <rebeca.cazacu@gmail.com>
2021-02-17 13:49:51 +0000

    add typeScript

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react'
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
 import { FaQuoteRight } from 'react-icons/fa'
 import data from './data'
+
 function App() {
-  const [people, setPeople] = useState(data)
+  const [people] = useState(data)
   const [index, setIndex] = useState(0)
 
   useEffect(() => {
